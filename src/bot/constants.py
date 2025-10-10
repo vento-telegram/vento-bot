6ca3bba05261433601bd32df9cf0b3d0e16f3e85
Andrei Bukhavets <andrei@bukhavets.com>
2025-10-11 01:54:44 +0300

    add mapping

--- src/bot/constants.py
+++ src/bot/constants.py
@@ -7,4 +7,5 @@ settings_models_mapper = {
     BotModeEnum.nano_banana: "nano_banana_price",
     BotModeEnum.suno_music: "suno_music_price",
     BotModeEnum.veo_video: "veo_standard_price",  # default maps to standard; improved selected in flow
+    BotModeEnum.sora2_video: "sora2_video_price",
 }
