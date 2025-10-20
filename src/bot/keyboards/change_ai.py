cf5d949e21bf1702ea3a1f72560025d736fc5edd
Andrei Bukhavets <andrei@bukhavets.com>
2025-10-20 17:33:14 +0300

    add sora 2 pro

--- src/bot/keyboards/change_ai.py
+++ src/bot/keyboards/change_ai.py
@@ -24,7 +24,7 @@ def mode_keyboard(active_mode: str | None = None) -> InlineKeyboardMarkup:
         ],
         [
             mode_button("Sora 2 PRO", "set_mode:sora2_pro_video", BotModeEnum.sora2_pro_video),
-        ]
+        ],
         [
             InlineKeyboardButton(text="Back", callback_data="goto:start"),
         ],
